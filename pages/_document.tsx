f4212124242cc6be5304d50701713e94e2da9799
Leonardo Barbosa <leonardo.barbosa@cbyk.com.br>
2020-10-12 19:27:40 -0300

    Adicionado google-site-verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,41 @@ class MyDocument extends Document<DocumentProps> {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="google-site-verification"
+            content="BN8GEQPzJ5R83WbppxtZMo5PDWOYGEs_QctH8a1VSN8"
+          />
+
+          <meta
+            property="og:image"
+            content="https://mndc.now.sh/images/logo.png"
+          />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#222222"
+          />
+          <meta name="msapplication-TileColor" content="#222222" />
+          <meta name="theme-color" content="#222222" />
         </Head>
         <body>
           <Main />
--- pages/_layouts/default.tsx
+++ pages/_layouts/default.tsx
@@ -22,38 +22,6 @@ const DefaultLayout: React.FC<IDefaultLayoutProps> = ({
       <meta property="og:site_name" content="Mande Coisas" />
       <meta property="og:title" content={`Mande Coisas ${title}`} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content="https://mndc.now.sh/images/logo.png" />
-
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#222222"
-      />
-      <meta name="msapplication-TileColor" content="#222222" />
-      <meta name="theme-color" content="#222222" />
-
-      <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap"
-        rel="stylesheet"
-      />
     </Head>
 
     <Grid
